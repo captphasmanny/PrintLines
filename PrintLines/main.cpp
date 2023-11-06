20c96e64410c4ca0b683bb475c4799a7758579e9
Manny <captphasmanny@att.net>
2023-11-06 10:46:16 -0500

    Playing around with inputs as well.

--- PrintLines/main.cpp
+++ PrintLines/main.cpp
@@ -12,6 +12,15 @@ int main()
     std::cout << "Hello" << " " << "Earthlings!" << '\n';
     std::cout << 4 << '\n';
     int x{ 5 };
-    std::cout << x <<'\n';
+    std::cout << x << std::endl;
     std::cout << "I will now print x, which is " << x << '\n';
+    std::cout << "I will now print x again, \nwhich is " << x << '\n';
+    //PREFER \n OVER std::endl
+    int inputY{};
+    std::cout << "Please enter a number: ";
+    std::cin >> inputY;
+    std::cout << "You entered: " << inputY << '\n';
+    std::cout << inputY << " + 9 = ";
+    std::cout << inputY + 9 << '\n';
 }
+
